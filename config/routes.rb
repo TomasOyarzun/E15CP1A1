ce1f5b136709df12148c189d2e09c1a2289b0a79
Tomas <oyarzun.tomas@gmail.com>
2018-09-24 20:24:01 -0300

    Fixed show histories from user with migration, routes and method create in histories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+	get 'histories/user_histories'
   resources :histories
   resource :sessions, only: [:create, :destroy]
   get 'users/sign_up', to: 'users#new'
   get 'users/sign_in', to: 'sessions#new'
   post 'users', to: 'users#create'
   root 'histories#index'
-  get 'histories/user_histories'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
